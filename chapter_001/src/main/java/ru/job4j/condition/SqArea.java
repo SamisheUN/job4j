87593fdbea7cea0a7e5edba2ff59dd31af4ebf5a
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-03-30 18:54:14 +0400

    Корректировка SqArea под задание

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,20 +1,32 @@
 package ru.job4j.condition;
 
-/*
+public class SqArea {
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
+    }
+}
+
+
+/*package ru.job4j.condition;
+
+
 2. Доработайте метод main с демонстрацией работы программы с параметрами p = 6, k = 2, площадь должна быть 2.
 h = p / (2 * (k + 1));
 L = h * k;
 s = L * h;
-*/
+
 
 public class SqArea {
     public static double square(int p, int k) {
-       /*
+
        int h = p / (2 * (k + 1));
        int l = h * k;
 
         return l * h;
-       */
+
         return (p / (2 * (k + 1))) * k * (p / (2 * (k + 1)));
     }
 
@@ -22,4 +34,4 @@ public class SqArea {
         double result1 = square(6, 2);
         System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
     }
-}
+} */
--- chapter_001/src/test/java/SqAreaTest.java
+++ chapter_001/src/test/java/SqAreaTest.java
@@ -1,5 +1,34 @@
 package ru.job4j.condition;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SqAreaTest {
+
+    @Test
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public void whenp6k2s2() {
+        int k = 2;
+        int p = 6;
+        int expected = 2;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+}
+
+
+/*package ru.job4j.condition;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,4 +42,4 @@ public class SqAreaTest {
         double out = 2;
         Assert.assertEquals(expected, out, 0.01);
      }
-}
+}*/
