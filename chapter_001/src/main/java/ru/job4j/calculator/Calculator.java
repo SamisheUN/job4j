64d2f6c1ec0498bf0aadddc183d66e7f89bed467
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-04-03 00:29:32 +0400

    5.8. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,33 +1,59 @@
 package ru.job4j.calculator;
 
-public class Calculator {
+/**
+ * Class класс для операций арифметических вычислений.
+ * @autor Samoylychev
+ * @since 01.04.2020
+ * @version 1
+ */
 
+public class Calculator {
+    /**
+     * Method add сложение
+     * @param first первое число
+     * @param second второе число
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
-
+    /**
+     * Method div деление
+     * @param first первое число
+     * @param second второе число
+     */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
-
+    /**
+     * Method multiply умножение
+     * @param first первое число
+     * @param second второе число
+     */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
-
+    /**
+     * Method subtrack вычитание
+     * @param first первое число
+     * @param second второе число
+     */
     public static void subtrack(double first, double second) {
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
 
+    /**
+     * Main - примеры работы методов с конкретными числами
+     * @param args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
         multiply(2, 1);
         subtrack(10, 5);
-
     }
 
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Samoylychev Andrey (SerSam-2007@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculator;
