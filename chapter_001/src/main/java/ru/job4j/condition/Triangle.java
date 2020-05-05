64dd89ba0882d4c31925368fdc5da762bbf6fd8a
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-05-06 00:21:03 +0400

    2.5. Инспектор кода IDEA. [#82295]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -5,11 +5,7 @@ public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
         boolean result = false;
 
-        if ((ab + bc) > ac && (ac + bc) > ab && (ab + ac) > bc) {
-            result = true;
-        } else {
-            result = false;
-        }
+        result = (ab + bc) > ac && (ac + bc) > ab && (ab + ac) > bc;
         return result;
     }
 }
--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,7 +2,8 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = false;
+        boolean prime;
+        prime = false;
         for (int index = 2; index <= number; index++) {
             if (number % index == 0 && index < number || number == 1) {
                 prime = false;
