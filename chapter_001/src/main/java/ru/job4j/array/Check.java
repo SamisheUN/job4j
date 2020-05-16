87ebbfd61e5a415942f558210bb3404f6435c570
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-05-16 22:27:53 +0400

    6.3. Массив заполнен true или false [#82316] Упрощён цикл for упрощён if за счёт проверки на несоответствие

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -5,14 +5,10 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (int index = 0; index < data.length; index++) {
-            if (data[0] != data[index]) {
+        for (boolean index : data) {
+            if (data[0] != index) {
                 result = false;
-                break;
-            } else {
-                result = true;
             }
-
         }
         return result;
     }
