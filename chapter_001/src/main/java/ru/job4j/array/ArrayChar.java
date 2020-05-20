5fc6d1c9a92c257799a0e9ed5f5ce37be5c27ad6
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-05-20 23:27:18 +0400

    6.4. Слово начинается с ... [#82315] дополнительный break.

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -7,7 +7,9 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
+
         boolean result = true;
+
         for (int index = 0; index < pref.length; index++) {
             //исходным в цикле взят массив pref,
             //т.к. условие не исключает наличие в word[] дополнительных ячеек
