8da5c564b5af75ac9518fd0fa014df32558b75b5
Andrey Solarsky <SerSam-2007@yandex.ru>
2020-07-06 08:33:22 +0400

    6.8. Удаление дубликатов в массиве. [#82319]

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index; /* указатель на null ячейку. */
+                /* переместить первую не null ячейку. Нужен цикл. */
+                for (int index2 = point + 1; index2 < array.length; index2++) { //цикл поиска !=0 далее point по массиву
+                    if (array[index2] != null) {
+                        array[point] = array[index2];
+                        array[index2] = null;
+                        break;
+                    }
+                }
+                }
+
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
