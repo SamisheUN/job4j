56c51e22ba1ce599f979da71d5d7eb7f22df58e7
SamisheUN <44730951+SamisheUN@users.noreply.github.com>
2026-04-07 23:25:35 +0400

    Add files via upload

---
+++ db/fauna/fauna.sql
@@ -0,0 +1,7 @@
+insert into fauna (name, avg_age, discovery_date) values ('makak', 1, '1020-09-01');
+insert into fauna (name, avg_age, discovery_date) values ('fish', 1000, '1030-09-01');
+insert into fauna (name, avg_age, discovery_date) values ('fishmakak', 10000, '2030-09-01');
+insert into fauna (name, avg_age, discovery_date) values ('makakf-ish', 10001, '1049-09-01');
+insert into fauna (name, avg_age, discovery_date) values ('fishfish', 15000, '9999-09-01');
+insert into fauna (name, avg_age, discovery_date) values ('fish-fish', 20000, '0001-09-01');
+insert into fauna (name, avg_age, discovery_date) values ('f-i-s-h', 20000, null);
---
+++ db/fauna/fauna_filter.sql
@@ -0,0 +1,8 @@
+-- Извлечение данных, у которых имя name содержит подстроку fish
+SELECT * FROM fauna WHERE name LIKE '%fish%';
+-- Извлечение данных, у которых средняя продолжительность жизни находится в диапазоне 10 000 и 21 000
+SELECT * FROM fauna WHERE avg_age < 20000 AND avg_age > 10000;
+-- Извлечение данных, у которых дата открытия не известна (null)
+SELECT * FROM fauna WHERE discovery_date IS NULL;
+-- Извлечение данных видов, у которых дата открытия раньше 1950 года
+SELECT * FROM fauna WHERE discovery_date > '1950-12-31';
